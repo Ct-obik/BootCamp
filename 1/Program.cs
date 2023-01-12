d7fb7843abbbb55886bc97ceca2cf8d0c89cb75d
Stanislav <ct-ob@mail.ru>
2023-01-12 18:22:42 +0300

    Find max sum of 3 numbers in massive

---
+++ 1/Program.cs
@@ -0,0 +1,19 @@
+﻿// дан массив и найти максимум из 3 подряд чисел этого массива
+
+int size = 10;
+int m = 3;
+int[] array = Enumerable.Range(1, size)
+                        .Select(item => Random.Shared.Next(10)) // случайные числа от 0 до 10
+                        .ToArray();
+
+Console.WriteLine(string.Join(' ', array));
+
+int max = 0;
+for (int j = 0; j < m; j++) max += array[j];
+int t = max;
+for (int i = 1; i < array.Length - m; i++)
+{
+    t = t - array[i - 1] + array[i + (m - 1)];
+    if (t > max) max = t;
+}
+Console.WriteLine(max);
